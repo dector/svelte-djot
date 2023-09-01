92334b3186958f4182bd4a2ad9f8dcedffd90606
Denys M <dector@dector.space>
2023-09-01 11:27:49 +0200

    feat: render HTML from djot source

---
+++ src/lib/Djot.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { parse, renderHTML } from '@djot/djot';
+
+	export let text = '';
+
+	const doc = parse(text);
+	const html = renderHTML(doc);
+</script>
+
+{@html html}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,3 @@
-// Reexport your entry components here
+import Djot from './Djot.svelte';
+
+export { Djot };
