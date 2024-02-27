788a4f54d52cd69503b7d49fc94ff7f9dec34521
Kelvin Soh <kelvinsjk@gmail.com>
2024-02-28 00:35:36 +0800

    feat: expose HTMLRenderOptions as prop

---
+++ src/routes/options/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Djot } from '$lib/index.js';
+  import type { renderHTML } from '@djot/djot';
+		
+	type HTMLRenderOptions = Parameters<typeof renderHTML>[1];
+
+	const text = '{-Hello-} {+Hola+} _djot_ from *SvelteKit*!';
+
+  const options: HTMLRenderOptions = {
+    overrides: {
+      emph: (node, renderer) => {
+        return `<span style="background-color: yellow;">${renderer.renderChildren(node)}</span>`
+      }
+    }
+  }
+</script>
+
+<Djot {text} {options} />
